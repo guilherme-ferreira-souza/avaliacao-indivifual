6effc37a69895dd3c9197ea1b0f8d02e4ffb6c6e
guilherme-ferreira-souza <80338511+guilherme-ferreira-souza@users.noreply.github.com>
2022-08-26 16:52:41 -0300

    commit parte 5

--- tutorial/mysite/polls/models.py
+++ tutorial/mysite/polls/models.py
@@ -10,6 +10,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,5 +21,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
     
 # Create your models here.
